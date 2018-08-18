4a609c7191d3fda6b2f875c07e2471c5e4e95a84
tranerius <36367029+tranerius@users.noreply.github.com>
2018-08-18 17:15:00 +0200

    Add container for words and texts

--- repetition_of_English_words/WordsAndTexts.cs
+++ repetition_of_English_words/WordsAndTexts.cs
@@ -3,37 +3,40 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows.Forms;
+using StringList = System.Collections.Generic.LinkedList<string>;
 
 [Serializable]
 public class WordsAndTexts
 {
-    public LinkedList<Dictionary<string, string>>[] Words { get; protected set; } // Ключ - английское слово, значение - русский перевод
-    public LinkedList<Dictionary<string, string>>[] Texts { get; protected set; }
+    public LinkedList<KeyValuePair<string, StringList>>[] Words { get; protected set; } // Ключ - английское слово, значение - связный список с русским переводом
+    public LinkedList<KeyValuePair<string, StringList>>[] Texts { get; protected set; }
 
     public WordsAndTexts()
     {
-        Words = new LinkedList<Dictionary<string, string>>[130];
-        Texts = new LinkedList<Dictionary<string, string>>[130];
+        Words = new LinkedList<KeyValuePair<string, StringList>>[500];
+        Texts = new LinkedList<KeyValuePair<string, StringList>>[100];
     }
 
-    public void AddWord(string word, string translation)
+    public void AddWord(string word, params string[] translation)
     {
         int index = 0;
         for (int i = 0; i < word.Length; ++i) index += word[i];
         index = index % Words.Length;
-        Dictionary<string, string> pairs = new Dictionary<string, string>();
-        pairs.Add(word, translation);
-        Words[index].AddLast(pairs);
+        StringList list = new StringList();
+        for (int i = 0; i < translation.Length; ++i) list.AddLast(translation[i]);
+        KeyValuePair<string, StringList> pair = new KeyValuePair<string, StringList>(word, list);
+        Words[index].AddLast(pair);
     }
 
-    public void AddText(string text, string translation)
+    public void AddText(string text, params string[] translation)
     {
         int index = 0;
         for (int i = 0; i < text.Length; ++i) index += text[i];
         index = index % Texts.Length;
-        Dictionary<string, string> pairs = new Dictionary<string, string>();
-        pairs.Add(text, translation);
-        Texts[index].AddLast(pairs);
+        StringList list = new StringList();
+        for (int i = 0; i < translation.Length; ++i) list.AddLast(translation[i]);
+        KeyValuePair<string, StringList> pair = new KeyValuePair<string, StringList>(text, list);
+        Texts[index].AddLast(pair);
     }
 
     public static void SerializeToFile(Stream file_stream, WordsAndTexts data)
