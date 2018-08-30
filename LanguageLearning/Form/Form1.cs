147f15d41cff03bf9f761893bbc3e7bc561a5ba5
tranerius <36367029+tranerius@users.noreply.github.com>
2018-08-30 23:17:41 +0200

    Мелкие правки

--- LanguageLearning/Form/Form1.cs
+++ LanguageLearning/Form/Form1.cs
@@ -38,12 +38,20 @@ public partial class Form1 : Form
         try
         {
             data_file = new FileStream("words.data", FileMode.Open);
+            if (data_file.Length == 0) throw new FileLoadException();
         }
         catch (FileNotFoundException)
         {
             DictionaryEditButton.Enabled = false;
             TrainingButton.Enabled = false;
-            MessageBox.Show("Файл со словарем не найден");
+            MessageBox.Show("Файл со словарем не найден", "Ошибка");
+            return;
+        }
+        catch (FileLoadException)
+        {
+            DictionaryEditButton.Enabled = false;
+            TrainingButton.Enabled = false;
+            MessageBox.Show("Открыт пустой файл", "Ошибка");
             return;
         }
 
@@ -87,11 +95,15 @@ public partial class Form1 : Form
     {
         if (Data != null)
         {
-            if (data_file == null) File.Create("words.data");
-            data_file.Seek(0, SeekOrigin.Begin);
-            byte[] buffer = new byte[data_file.Length];
-            data_file.Read(buffer, 0, buffer.Length);
-            data_file.SetLength(0);
+            if (data_file == null) data_file = File.Create("words.data");
+            byte[] buffer = null;
+            if (data_file.Length > 0)
+            {
+                data_file.Seek(0, SeekOrigin.Begin);
+                buffer = new byte[data_file.Length];
+                data_file.Read(buffer, 0, buffer.Length);
+                data_file.SetLength(0);
+            }
             try
             {
                 Serialize.SerializeToFile(data_file, new Container(Data.Words, Data.Texts, Incomprehensible));
@@ -99,7 +111,8 @@ public partial class Form1 : Form
             catch (SerializationException)
             {
                 data_file.SetLength(0);
-                data_file.Write(buffer, 0, buffer.Length);
+                if (buffer != null) data_file.Write(buffer, 0, buffer.Length);
+                else data_file.SetLength(0);
             }
             finally
             {
